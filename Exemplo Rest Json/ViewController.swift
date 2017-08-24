00a8b3e3bdee265e2082165f2416ce990a5fdaf5
Usuário Convidado <Guest@l2602micro61.local>
2017-08-24 19:39:34 -0300

    Initial Commit

---
+++ Exemplo Rest Json/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exemplo Rest Json
+//
+//  Created by Usuário Convidado on 24/08/17.
+//  Copyright © 2017 Agesandro Scarpioni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
