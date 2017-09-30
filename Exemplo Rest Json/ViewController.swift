ded15f94b82d1d92fdec5af6453e04e34b6242f7
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-09-29 21:46:49 -0300

    Exemplo rest json

--- Exemplo Rest Json/ViewController.swift
+++ Exemplo Rest Json/ViewController.swift
@@ -9,6 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var minhaImagem: UIImageView!
+    @IBOutlet weak var local: UILabel!
+    @IBOutlet weak var estado: UILabel!
+    
+    var session: URLSession?
+    
+    @IBAction func exibir(_ sender: Any) {
+        let config = URLSessionConfiguration.default
+        
+        session = URLSession(configuration: config)
+        let url = URL(string: "https://parks-api.herokuapp.com/parks/577024e4a44821110001ee93")
+        
+        let task = session?.dataTask(with: url!, completionHandler: { (data, response, error) in
+            //ações que serão efetuadas quando
+            //a execução da task se completa
+            let texto = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print(texto!)
+            if let nPQ = self.retornarNomePq(data: data!){
+                DispatchQueue.main.async {
+                    self.local.text = nPQ
+                }
+            }
+            
+            if let ePQ = self.retornarEstadoPq(data: data!){
+                DispatchQueue.main.async {
+                    self.estado.text = ePQ
+                }
+            }
+            
+            
+            
+        })
+        //a linha abaixo dispara a execução da task
+        task?.resume()
+    }
+    
+    func retornarNomePq(data: Data) -> String? {
+        var resposta:String?=nil
+        do{
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+            if let nomeParque = json["nome"] as? String{
+                resposta = nomeParque
+            }
+            
+        }catch let error as NSError{
+            return "Falha ao carregar: \(error.localizedDescription)"
+        }
+        return resposta
+    }
+    
+    func retornarEstadoPq(data: Data) -> String? {
+        var resposta:String?=nil
+        do{
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+            if let nomeEstado = json["estado"] as? String{
+                resposta = nomeEstado
+            }
+            
+        }catch let error as NSError{
+            return "Falha ao carregar: \(error.localizedDescription)"
+        }
+        return resposta
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
